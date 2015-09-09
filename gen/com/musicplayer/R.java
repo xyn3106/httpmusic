43266bd0b8d12b874d7c2e245379c5de68fef493
xyn <dc.xyn@qq.com>
2015-09-09 22:38:47 +0800

    加url框

--- gen/com/musicplayer/R.java
+++ gen/com/musicplayer/R.java
@@ -11,21 +11,38 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int bt_next_nor=0x7f020000;
+        public static final int bt_next_press=0x7f020001;
+        public static final int bt_pause_nor=0x7f020002;
+        public static final int bt_pause_press=0x7f020003;
+        public static final int bt_play_nor=0x7f020004;
+        public static final int bt_play_press=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int selector_btn_next=0x7f020007;
+        public static final int selector_btn_pause=0x7f020008;
+        public static final int selector_btn_play=0x7f020009;
     }
     public static final class id {
-        public static final int btnPause=0x7f070004;
-        public static final int btnPlayUrl=0x7f070003;
-        public static final int btnStop=0x7f070005;
-        public static final int home_foot=0x7f070007;
-        public static final int home_girl_iv=0x7f070002;
-        public static final int home_news_iv=0x7f070001;
-        public static final int home_phone_iv=0x7f070000;
-        public static final int skbProgress=0x7f070006;
+        public static final int bottom_btn_next=0x7f070004;
+        public static final int bottom_btn_play=0x7f070005;
+        public static final int bottom_img_image=0x7f070002;
+        public static final int bottom_img_shadow=0x7f070000;
+        public static final int bottom_tv_artist=0x7f070003;
+        public static final int bottom_tv_title=0x7f070001;
+        public static final int btnPause=0x7f07000c;
+        public static final int btnPlayUrl=0x7f07000b;
+        public static final int edit_text=0x7f07000a;
+        public static final int home_foot=0x7f07000e;
+        public static final int home_girl_iv=0x7f070008;
+        public static final int home_news_iv=0x7f070007;
+        public static final int home_phone_iv=0x7f070006;
+        public static final int phone=0x7f070009;
+        public static final int skbProgress=0x7f07000d;
     }
     public static final class layout {
-        public static final int include_home_tabfoot=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int bottombar=0x7f030000;
+        public static final int include_home_tabfoot=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class raw {
         public static final int girl_normal=0x7f040000;
--- src/com/musicplayer/test_musicplayer.java
+++ src/com/musicplayer/test_musicplayer.java
@@ -1,16 +1,19 @@
 package com.musicplayer;  
   
 import android.app.Activity;  
-import android.os.Bundle;  
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;  
 import android.view.View.OnClickListener;  
 import android.widget.Button;  
-import android.widget.SeekBar;  
+import android.widget.SeekBar;
+import android.widget.Toast;  
   
 public class test_musicplayer extends Activity {  
     private Button btnPause, btnPlayUrl, btnStop;  
     private SeekBar skbProgress;  
     private Player player;  
+    private long exitTime = 0;
     
     boolean canplay=true;
   
@@ -28,26 +31,41 @@ public class test_musicplayer extends Activity {
         btnPause = (Button) this.findViewById(R.id.btnPause);  
         btnPause.setOnClickListener(new ClickEvent());  
   
-        btnStop = (Button) this.findViewById(R.id.btnStop);  
-        btnStop.setOnClickListener(new ClickEvent());  
+//        btnStop = (Button) this.findViewById(R.id.btnStop);  
+//        btnStop.setOnClickListener(new ClickEvent());  
  
         skbProgress = (SeekBar) this.findViewById(R.id.skbProgress);  
         skbProgress.setOnSeekBarChangeListener(new SeekBarChangeEvent());  
         player = new Player(skbProgress);  
-    }  
+    }
   
 //    public void onStop(){
 //    	super.onStop();
 //    	System.exit(0);
 //    }
     
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){   
+	        if((System.currentTimeMillis()-exitTime) > 2000){  
+	            Toast.makeText(getApplicationContext(), "再按一次退出!", Toast.LENGTH_SHORT).show();                                
+	            exitTime = System.currentTimeMillis();   
+	        } else {
+	            finish();
+	            System.exit(0);
+	        }
+	        return true;   
+	    }
+		return super.onKeyDown(keyCode, event);
+	}
+    
     class ClickEvent implements OnClickListener {  
   
         @Override  
         public void onClick(View arg0) {  
-        	if (arg0 == btnPause) {  
-                player.pause();  
-            } else if (arg0 == btnPlayUrl) {  
+        	if (arg0 == btnPause) {
+                player.pause();
+            } else if (arg0 == btnPlayUrl) {
             	if(canplay==true)
             	{
                 String url="http://www.kfybsf.com/mp3/liangzhu.mp3";
@@ -57,11 +75,11 @@ public class test_musicplayer extends Activity {
             	}
             	else
             		player.play();
-            } else if (arg0 == btnStop) {  
-                player.stop();  
+            } else if (arg0 == btnStop) {
+                player.stop();
                 canplay=true;
             }
-        }  
+        }
     }  
   
     class SeekBarChangeEvent implements SeekBar.OnSeekBarChangeListener {  
